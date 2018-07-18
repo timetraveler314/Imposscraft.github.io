3fc2aede038b2264ee36a78f46e033e36406ffb2
Imposscraft <33189830+Imposscraft@users.noreply.github.com>
2018-07-18 21:43:03 +0800

    Update main.js

--- win10/main.js
+++ win10/main.js
@@ -1,14 +1,16 @@
-function showmenu(){
+$(document).ready(function(){
+$("#menubar").ready(function(){
 	if($('#menubar').css('display') =='block'){
 		$('#menubar').css('display','none');
 	} else {
 		$('#menubar').css('display','block');
-	}
-}
-function showwnd_pc(){
+	}}
+);
+$("#wnd_pc").ready(function(){
 	if($('#wnd_pc').css('display') =='block'){
 		$('#wnd_pc').css('display','none');
 	} else {
 		$('#wnd_pc').css('display','block');
-	}
-}
+	}}
+);
+});
